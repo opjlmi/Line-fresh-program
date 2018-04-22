f7c7a4ca5b17f8e90cc4eaf5718b5acb5d19f477
opjlmi <opjlmi@gmail.com>
2018-04-23 01:17:57 +0800

    Import sdk and init sdk keys

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const _wit = require('node-wit');
+const _line = require('@line/bot-sdk');
+const _translate = require('@google-cloud/translate');
+const key = require('../webhookkeys');
+
+// Key Cofigure
+const wit = new _wit.Wit({
+  accessToken: key.witai.key,
+});
+
+const line = new _line.Client({
+  channelAccessToken: key.line.channelAccessToken,
+  channelSecret: key.line.channelSecret,
+});
+
+const translate = new _translate({
+  key: key.translate.key,
+});
 
 router.get('/', (req, res, next) => {
   res.status(404);
